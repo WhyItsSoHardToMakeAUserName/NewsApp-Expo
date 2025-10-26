a6800f1fae453556acdbf851b474f9b274d12d82
WhyItsSoHardToMakeAUserName <86366420+WhyItsSoHardToMakeAUserName@users.noreply.github.com>
2025-10-26 15:32:05 +0500

    added search

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,8 +2,8 @@ import NewsCard from "@/components/news-card";
 import { usePushNotifications } from "@/hooks/use-push-notifications";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import * as Notifications from "expo-notifications";
-import React, { useEffect, useRef } from "react";
-import { ActivityIndicator, FlatList } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { ActivityIndicator, FlatList, StyleSheet, TextInput, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 type Article = {
@@ -24,14 +24,22 @@ export default function HomeScreen() {
   const apiBaseUrl = "https://newsapi.org/v2/everything?";
   usePushNotifications();
 
+  const [query, setQuery] = useState("technology");
+  const [searchText, setSearchText] = useState(query);
+
+  const handleSearch = () => {
+    setQuery(searchText);
+    refetch();
+  };
+
   const fetchNews = async ({ pageParam = new Date().toISOString() }): Promise<NewsResponse> => {
     const res = await fetch(
-      `${apiBaseUrl}q=technology&sortBy=publishedAt&to=${pageParam}&pageSize=20&apiKey=${apiKey}`
+      `${apiBaseUrl}q=${encodeURIComponent(query)}&sortBy=publishedAt&to=${pageParam}&pageSize=20&apiKey=${apiKey}`
     );
     return res.json();
   };
 
-  const { data, isLoading, fetchNextPage, hasNextPage, isFetchingNextPage } = useInfiniteQuery({
+  const { data, isLoading, fetchNextPage, hasNextPage,refetch, isFetchingNextPage } = useInfiniteQuery({
     queryKey: ["news"],
     queryFn: fetchNews,
     initialPageParam: new Date().toISOString(),
@@ -59,6 +67,18 @@ export default function HomeScreen() {
 
   return (
     <SafeAreaView style={{ flex: 1, paddingHorizontal: 10 }}>
+      {/* Search bar */}
+      <View style={styles.searchContainer}>
+        <TextInput
+          value={searchText}
+          onChangeText={setSearchText}
+          placeholder="Search news..."
+          onSubmitEditing={handleSearch}
+          style={styles.searchInput}
+          returnKeyType="search"
+        />
+      </View>
+
       {/* News list */}
       <FlatList
         data={data?.pages.flatMap((page) => page.articles) || []}
@@ -81,3 +101,19 @@ export default function HomeScreen() {
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    marginBottom: 10,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  searchInput: {
+    flex: 1,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    backgroundColor: "#fff",
+  },
+});
